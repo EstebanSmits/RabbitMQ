a924b847fcfd4e0fb88efd571f2a2c6aac1c477f
daxmax <daxmax@gmail.com>
2018-10-24 02:06:55 -0500

    first commit of a test project to review RabbitMQ capability

---
+++ subscriber1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EasyNetQ;
+using Messages;
+
+namespace subscriber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+               var subscriptionResult= bus.Subscribe<TextMessage>("", HandleTextMessage);
+            
+                bool _quitFlag =false;
+                Console.WriteLine("Listening for messages.Ctrl+C to exit");
+                while(!_quitFlag)
+                {
+                    var keyInfo = Console.ReadKey();
+                    _quitFlag = keyInfo.Key == ConsoleKey.C && keyInfo.Modifiers == ConsoleModifiers.Control;
+                }
+            }
+            
+
+
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+        }
+    }
+}
---
+++ webapi/Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Messages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        
+        // POST api/values
+        [HttpPost]
+        [ProducesResponseType(typeof (string),(int) HttpStatusCode.OK)]
+        public void Post([FromBody] string message)
+        {
+        using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+            
+                    bus.Publish(new TextMessage
+                        {
+                            Text = message
+                        });
+                }
+        }
+
+        
+    }
+}
